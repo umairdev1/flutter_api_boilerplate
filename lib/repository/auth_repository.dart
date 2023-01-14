97d9a8527fdb93854dac8f003eaed9b815cfaa96
umairsaleem623 <umairsaleem623@gmail.com>
2023-01-14 15:55:32 +0500

    init

---
+++ lib/repository/auth_repository.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_api_boilerplate/data/network/base_api_services.dart';
+import 'package:flutter_api_boilerplate/data/network/network_api_services.dart';
+
+import '../resources/components/app_urls.dart';
+
+class AuthRepository {
+  BaseApiServices _apiServices = NetworkApiService();
+
+  Future<dynamic> loginApi(dynamic data) async {
+    try {
+      dynamic response =
+          await _apiServices.getPostApiResponse(AppUrl.loginUrl, data);
+
+      return response;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<dynamic> registerApi(dynamic data) async {
+    try {
+      dynamic response =
+          await _apiServices.getPostApiResponse(AppUrl.registeredUrl, data);
+
+      return response;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
